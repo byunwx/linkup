b73d9f0bfba7ee05a0f8f7ba7ae30182dabc997c
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-23 22:16:49 -0400

    finished newlink.js to get value from the user's entry and routed the data to api/link/data.

--- public/assets/js/newlink.js
+++ public/assets/js/newlink.js
@@ -5,7 +5,47 @@ $(document).ready(function() {
   const siteDescription = $("#site-description");
   const notShared = ("#not-shared");
 
+newLinkForm.on("submit", function(event) {
+  console.log("submited first");
+  event.preventDefault();
+
+  function submitFinal() {
+    console.log("called second");
+    const linkData = {
+      title: titleInput.val().trim(),
+      url: urlInput.val().trim(),
+      description: siteDescription.val(),
+      shared: notShared.val()
+    };
+
+    // if (!linkData.url) {
+    //   return;
+    // }
+    enterLink(linkData.title, Data.url, linkData.description, linkData.shared);
+    titleInput.val("");
+    urlInput.val("");
+    siteDescription.val("");
+    notShared.val("");
+  }
+  submitFinal();
+});
+
+function enterLink(title, url, description, shared) {
+  console.log("called third");
+  $.post("api/link/data", {
+    title: title,
+    url: url,
+    description: description,
+    shared: shared
+  }).then(data => {
+    console.log(data);
+  }).catch(handleSubmitError);
+}
+
+function handleSubmitError(err) {
+  $("#alert .msg").text(err.responseJSON);
+  $("#alert").fadeIn(500);
+}
 
 
-
-}) // end of document.ready
+}); // end of document.ready
