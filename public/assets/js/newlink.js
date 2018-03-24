c75d9b29095ddec25c3f75b9c8fc2a240ac35901
Adrian Kyzer <adriankyzer@gmail.com>
2018-03-23 21:14:24 -0400

    created modal to add new links and started newlink.js for it

---
+++ public/assets/js/newlink.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  const newLinkForm = $(".add-link");
+  const titleInput = $("#site-title");
+  const urlInput = $("#site-url");
+  const siteDescription = $("#site-description");
+  const notShared = ("#not-shared");
+})
