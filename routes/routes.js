6b0e0d21cc1d70c740f6d900de8a23e1610ffbee
jongtelles <jongtelles@gmail.com>
2018-03-26 15:35:13 -0400

    Formatting

--- routes/routes.js
+++ routes/routes.js
@@ -6,39 +6,40 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = app => {
 
-  app.get("/", (req,res)=>{
-      if (req.user){
-          res.redirect("/home")
-      }
-      else{
-      let placeholder;//will be redifined through development
-      res.render("landing", placeholder)
-    }
-  });
+    app.get("/", (req, res) => {
+        if (req.user) {
+            res.redirect("/home")
+        } else {
+            let placeholder; //will be redifined through development
+            res.render("landing", placeholder)
+        }
+    });
 
 
-  app.get("/search", isAuthenticated, (req,res)=>{
-      let placeholder;//will be redifined through development
-      res.render("search", placeholder)
-  });
+    app.get("/search", isAuthenticated, (req, res) => {
+        let placeholder; //will be redifined through development
+        res.render("search", placeholder)
+    });
 
 
-  app.get("/home", isAuthenticated, (req, res) => {
-          let links = {};
-          res.render("home", links);
-          // res.json(data) // will be edited to not display user password
-  })
+    app.get("/home", isAuthenticated, (req, res) => {
+        let links = {};
+        res.render("home", links);
+        // res.json(data) // will be edited to not display user password
+    })
 
 
-  app.get("/user/:userid", (req,res)=>{
-      if (!req.user){
-          res.redirect("/");
-      }
-      db.User.findOne({
-              include: [db.Link],
-              where: {id:req.params.id}
-      }).then((data)=>{
-          res.render("user", data)
-      })
-  });
-  }
+    app.get("/user/:userid", (req, res) => {
+        if (!req.user) {
+            res.redirect("/");
+        }
+        db.User.findOne({
+            include: [db.Link],
+            where: {
+                id: req.params.id
+            }
+        }).then((data) => {
+            res.render("user", data)
+        })
+    });
+}
