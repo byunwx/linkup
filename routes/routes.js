3c5914885ef29a8e21fdf5d2f274fc3b0c4874e9
jongtelles <jongtelles@gmail.com>
2018-03-26 10:30:43 -0400

    Home and Link Handlebars

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,39 @@
+'use strict';
 const path = require("path");
 const db = require("../models");
 
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = app => {
-app.get("/", (req,res) =>{
-    let placeholder; //will be redifined through development
-    if (req.user){
-        res.redirect("/home")
-    }
-    // res.render("landing", placeholder)
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-});
+    app.get("/", (req, res) => {
+        // let placeholder; //will be redifined through development
+        if (req.user) {
+            res.redirect("/home");
+        }
+        // res.render("landing", placeholder)
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
 
-app.get("/home", isAuthenticated, (req,res)=>{
-    let placeholder;//will be redifined through development
-    res.render("home", placeholder)
-});
-app.get("/user/:userid", (req,res)=>{
-    if (!req.user){
-        res.redirect("/");
-    }
-    db.User.findOne({
-            include: [db.Link],
-            where: {id:req.params.id}
-    }).then((data)=>{
-        res.render("user", data)
+    app.get("/home", isAuthenticated, (req, res) => {
+        let links;
+        app.get("/api/link/data", (req, res) => {
+            links = res;
+        });
+        res.render("home", links);
     })
-});
+
+
+    app.get("/user/:userid", (req, res) => {
+        if (!req.user) {
+            res.redirect("/");
+        }
+        db.User.findOne({
+            include: [db.Link],
+            where: {
+                id: req.params.id
+            }
+        }).then((data) => {
+            res.render("user", data);
+        })
+    });
 }
