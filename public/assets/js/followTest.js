c5d56f71d412cc1a866f9aee42b77f0d36051e1e
Marucs Hilaire <marcushilaire@The-Mac.local>
2018-03-27 13:39:50 -0400

    you can only follow a user one time

--- public/assets/js/followTest.js
+++ public/assets/js/followTest.js
@@ -10,50 +10,58 @@ function updatePost(post) {
       });
   }
   $("#follow").on("click", function(){
-      callTest(1,2)
+      callTest(2)
   })
-function callTest(follower, followee){
+function callTest(followee){
     console.log("test")
     //update follower (current user)
     $.get("/api/user/data", function(data) {
-    let arrTest={}
-    console.log("this is the follower data")
-    console.log(JSON.parse(data.array));
-    console.log("======")
-    if (data.array){
-    arrTest=JSON.parse(data.array)
-    } else {
-        arrTest = {
-            following:[],
-            followers:[]
-        }
-    }
-    arrTest.following.push(followee)
-    var newPost = {
-        array:JSON.stringify(arrTest)
-    };
-    newPost.id = follower;
-    updatePost(newPost)      
-    });
-    // update the followee (user being followed)
-    $.get("/api/user/"+followee, function(data){
         let arrTest={}
-        console.log("this is the followee data")
+        console.log("this is the follower data")
         console.log(JSON.parse(data.array));
-        console.log("=======")
-        if(data.array){
-            arrTest=JSON.parse(data.array)
-        }else {
-            arrTest={
+        console.log("======")
+        if (data.array){
+        arrTest=JSON.parse(data.array)
+        } else {
+            arrTest = {
                 following:[],
                 followers:[]
             }
-        }
-        arrTest.followers.push(follower)
+        }//end of JSON Parse if else
+        if(arrTest.following.includes(followee)){
+            console.log("you already follow this user")
+            return
+        }else{
+        arrTest.following.push(followee)
         var newPost = {
-            array: JSON.stringify(arrTest)
+            array:JSON.stringify(arrTest)
         };
-        newPost.id =followee
-        updatePost(newPost)
-    })
+        newPost.id = data.id;
+        updatePost(newPost);  
+        updateFollowee(followee,newPost.id)    
+        }
+    });
+    // update the followee (user being followed)
+    function updateFollowee(followee,follower){
+        $.get("/api/user/"+followee, function(data){
+            let arrTest={}
+            console.log("this is the followee data")
+            console.log(JSON.parse(data.array));
+            console.log("=======")
+            if(data.array){
+                arrTest=JSON.parse(data.array)
+            }else {
+                arrTest={
+                    following:[],
+                    followers:[]
+                }
+            }
+            arrTest.followers.push(follower)
+            var newPost = {
+                array: JSON.stringify(arrTest)
+            };
+            newPost.id =followee
+            updatePost(newPost)
+        })
+    }
 }
